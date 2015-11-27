3173af83e2b4139b403298cd6869d04e61833288
Ben Foxall <ben@whiteoctober.co.uk>
2015-11-27 21:06:15 +0000

    more store stuff, before we make it weird

--- store.js
+++ store.js
@@ -1,9 +1,12 @@
 function Store(size, n) {
   this._data = [];
+  this._size = size;
 }
 
 Store.prototype.add = function(i) {
   this._data.unshift(i);
+  if (this._data.length > this._size)
+    this._data.pop();
 };
 
 Store.prototype.last = function(i) {
@@ -11,6 +14,7 @@ Store.prototype.last = function(i) {
 };
 
 Store.prototype.take = function(c, fn) {
+  c = Math.min(c, this._data.length);
   for (var i = 0; i < c; i++) {
     fn(this._data[i]);
   }
--- test/tests.js
+++ test/tests.js
@@ -3,10 +3,10 @@ var expect = chai.expect;
 describe('Store', function() {
   var store;
 
-  describe('size 10', function() {
+  describe('size 5', function() {
 
     before(function() {
-      store = new Store(10, 1);
+      store = new Store(5, 1);
       store.add(5);
       store.add(6);
       store.add(7);
@@ -24,6 +24,47 @@ describe('Store', function() {
       });
 
       expect(items).to.eql([7, 6, 5]);
+
+    });
+
+    it('only gives amount of items stored', function() {
+      var items = [];
+
+      store.take(10, function(i) {
+        items.push(i);
+      });
+
+      expect(items).to.eql([7, 6, 5]);
+
+    });
+
+    it('rolls on more than 5 items', function() {
+      store.add(8);
+      store.add(9);
+      store.add(10);
+
+      var items = [];
+
+      store.take(10, function(i) {
+        items.push(i);
+      });
+
+      expect(items).to.eql([10, 9, 8, 7, 6]);
+
+    });
+
+    it('is pretty cool with stuff', function() {
+      for (var i = 0; i < 1000; i++) {
+        store.add(7);
+      }
+
+      var items = [];
+
+      store.take(10, function(i) {
+        items.push(i);
+      });
+
+      expect(items).to.eql([7, 7, 7, 7, 7]);
 
     });
 
