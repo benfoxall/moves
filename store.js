d57894c30ecb1eed21918e9941e6f0b3f0511be7
Ben Foxall <ben@whiteoctober.co.uk>
2015-11-28 11:32:30 +0000

    support multi item store

--- store.js
+++ store.js
@@ -1,9 +1,12 @@
 function Store(size, n) {
   this._data = [];
   this._size = size;
+  this._n = n || 1;
 }
 
 Store.prototype.add = function(i) {
+  if(!Array.isArray(i)) i = [i];
+
   this._data.unshift(i);
   if (this._data.length > this._size)
     this._data.pop();
@@ -12,20 +15,36 @@ Store.prototype.add = function(i) {
 Store.prototype.take = function(c, fn) {
   c = Math.min(c, this._data.length);
   for (var i = 0; i < c; i++) {
-    fn(this._data[i]);
+    fn.apply(this,this._data[i]);
   }
 };
 
 Store.prototype.extent = function(i) {
-  var min, max;
+  var _n = this._n;
+  var min = new Array(_n), max = new Array(_n);
+
   this.take(i, function(value){
-    min = min ? Math.min(min, value) : value;
-    max = max ? Math.max(max, value) : value;
+    for (var j = 0; j < _n; j++) {
+      min[j] = min[j] ? Math.min(min[j], arguments[j]) : arguments[j];
+      max[j] = max[j] ? Math.max(max[j], arguments[j]) : arguments[j];
+    }
   })
-  return [min,max]
+
+  return min.map(function(min, i){
+    return [min, max[i]]
+  });
 }
 
 Store.prototype.distance = function(i) {
+
   var ex = this.extent(i);
-  return ex ? ex[1] - ex[0] : 0;
+
+  var vectors = ex.map(function(e){
+    return e[1] - e[0]
+  })
+
+  return Math.sqrt(vectors.reduce(function(memo, v){
+    return Math.pow(v, 2) + memo;
+  },0))
+
 }
--- test/tests.js
+++ test/tests.js
@@ -5,7 +5,9 @@ describe('Store', function() {
     var items = [];
 
     store.take(i, function(i) {
-      items.push(i);
+      var args = [].slice.call(arguments);
+      if(args.length === 1) items.push(i)
+      else items.push(args);
     });
 
     return items;
@@ -52,11 +54,61 @@ describe('Store', function() {
     });
 
     it('computes extent', function() {
-      expect(store.extent(10)).to.eql([5,7])
+      expect(store.extent(10)).to.eql([[5,7]])
     });
 
     it('computes distance', function() {
       expect(store.distance(10)).to.eql(2)
+    });
+
+  });
+
+
+  describe('size 5, n=2', function() {
+
+    beforeEach(function() {
+      store = new Store(5, 2);
+      store.add([5,5]);
+      store.add([6,6]);
+      store.add([7,7]);
+    });
+
+    it('iterates through', function() {
+
+      expect(take(3)).to.eql([[7,7], [6,6], [5,5]]);
+
+    });
+
+    it('only gives amount of items stored', function() {
+
+      expect(take(10)).to.eql([[7,7], [6,6], [5,5]]);
+
+    });
+
+    it('rolls on more than 5 items', function() {
+      store.add([8, 8]);
+      store.add([9, 9]);
+      store.add([10, 10]);
+
+      expect(take(10)).to.eql([[10,10], [9,9], [8,8], [7,7], [6,6]]);
+
+    });
+
+    it('is pretty cool with stuff', function() {
+      for (var i = 0; i < 1000; i++)
+        store.add([7,7]);
+
+      expect(take(10)).to.eql([[7,7], [7,7], [7,7], [7,7], [7,7]]);
+
+    });
+
+    it('computes extent', function() {
+      console.log(store.extent(10))
+      expect(store.extent(10)).to.eql([[5,7],[5,7]])
+    });
+
+    it('computes distance', function() {
+      expect(store.distance(10)).to.eql(Math.sqrt(8))
     });
 
   });
