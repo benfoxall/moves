4990ba398c01938ca67b1c36f92f6f0ed8b0d43e
Ben Foxall <ben@whiteoctober.co.uk>
2015-12-03 18:26:46 +0000

    jscs whatever

--- store.js
+++ store.js
@@ -1,4 +1,4 @@
-function TimeStore(n){
+function TimeStore(n) {
   this.n = n || 1;
   this.size = 512;
   this.data = new Int32Array(this.size * this.n);
@@ -8,39 +8,39 @@ function TimeStore(n){
   this.last = this.now();
 }
 
-TimeStore.prototype.now = function(){
+TimeStore.prototype.now = function() {
   return Date.now();
-}
+};
 
-TimeStore.prototype.add = function(){
+TimeStore.prototype.add = function() {
 
   this.idx = (this.idx || this.size) - 1;
 
   for (var i = 0; i < this.n; i++)
-    this.data[(this.idx*this.n) + i] = arguments[i];
+    this.data[(this.idx * this.n) + i] = arguments[i];
 
-  this.count ++;
+  this.count++;
 
   // store the timestamp diff of this event
   var now = this.now();
   var delta = now - this.last;
   this.deltas[this.idx] = delta;
   this.last = now;
-}
+};
 
-TimeStore.prototype.each = function(milliseconds, fn){
+TimeStore.prototype.each = function(milliseconds, fn) {
   milliseconds -= (this.now() - this.last);
 
   var args = new Array(this.n);
 
   var offset = 0, offsetMax = Math.min(this.count, this.size);
-  while(offset < offsetMax){
-    if(milliseconds<0) break;
+  while (offset < offsetMax) {
+    if (milliseconds < 0) break;
 
     var i = (this.idx + offset) % this.size;
 
     for (var j = 0; j < this.n; j++) {
-      args[j] = this.data[(i*this.n) + j];
+      args[j] = this.data[(i * this.n) + j];
     }
 
     fn.apply(null, args);
@@ -48,21 +48,20 @@ TimeStore.prototype.each = function(milliseconds, fn){
     milliseconds -= this.deltas[i];
     offset++;
   }
-}
+};
 
-
-TimeStore.prototype.extent = function(milliseconds){
+TimeStore.prototype.extent = function(milliseconds) {
   milliseconds -= (this.now() - this.last);
 
   var min = new Array(this.n), max = new Array(this.n), i, v;
   var offset = 0, offsetMax = Math.min(this.count, this.size);
-  while(offset < offsetMax){
-    if(milliseconds<0) break;
+  while (offset < offsetMax) {
+    if (milliseconds < 0) break;
 
     i = (this.idx + offset) % this.size;
 
     for (var j = 0; j < this.n; j++) {
-      v = this.data[(i*this.n) + j];
+      v = this.data[(i * this.n) + j];
       min[j] = offset ? Math.min(min[j], v) : v;
       max[j] = offset ? Math.max(max[j], v) : v;
     }
@@ -74,19 +73,16 @@ TimeStore.prototype.extent = function(milliseconds){
   return min.map(function(min, i) {
     return [min, max[i]];
   });
-}
+};
 
-TimeStore.prototype.distance = function(milliseconds){
+TimeStore.prototype.distance = function(milliseconds) {
   return Math.sqrt(
     this.extent(milliseconds)
       .reduce(function(memo, e) {
         return Math.pow(e[1] - e[0], 2) + memo;
       },0)
   );
-}
-
-
-
+};
 
 function Store(size, n) {
   this.size = size || 128;
@@ -100,7 +96,7 @@ function Store(size, n) {
 }
 
 Store.prototype.add = function(i) {
-  if (!Array.isArray(i)) {i = [i];};
+  if (!Array.isArray(i)) {i = [i];}
 
   // overflow possible
   this.data.set(i, this.i * this.n);
