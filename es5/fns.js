f3122c65a76dc1e9caa1e607f59ea9a1bca210f2
Ben Foxall <ben@whiteoctober.co.uk>
2015-12-03 21:17:31 +0000

    Wrap device orientation properties

--- es5/fns.js
+++ es5/fns.js
@@ -1,13 +1,21 @@
 "use strict";
 
 var distance = function distance(_) {
-    return Math.sqrt(_.map(function (x) {
-        return x * x;
-    }).reduce(function (a, b) {
-        return a + b;
-    }, 0));
+  return Math.sqrt(_.map(function (x) {
+    return x * x;
+  }).reduce(function (a, b) {
+    return a + b;
+  }, 0));
 };
 
 var colour = function colour(i) {
-    return "rgb(" + Math.floor(i * 255) + ", " + Math.floor((1 - i) * 255) + ", 0)";
+  return "rgb(" + Math.floor(i * 255) + ", " + Math.floor((1 - i) * 255) + ", 0)";
+};
+
+var wrap = function wrap(e) {
+  return {
+    alpha: Math.sin(Math.PI * (e.alpha / 360)),
+    beta: Math.sin(Math.PI * (e.beta / 360)),
+    gamma: Math.sin(Math.PI * (e.gamma / 180))
+  };
 };
--- src/fns.js
+++ src/fns.js
@@ -3,5 +3,12 @@ let distance = _ => Math.sqrt(_
     .reduce((a, b) => a + b, 0)
    )
 
-
 let colour = i => `rgb(${Math.floor(i * 255)}, ${Math.floor((1-i) * 255)}, 0)`
+
+let wrap = e => {
+  return {
+    alpha: Math.sin(Math.PI*(e.alpha/360)),
+    beta:  Math.sin(Math.PI*(e.beta /360)),
+    gamma: Math.sin(Math.PI*(e.gamma/180))
+  }
+}
